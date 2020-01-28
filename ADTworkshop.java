4e9ef8ceb34b842c782393192b970f340f5bfa26
Oralita <54722120+oat431@users.noreply.github.com>
2020-01-28 21:41:17 +0700

    ADT workshop edited
    change filename and put some comment on it

--- ADTworkshop.java
+++ ADTworkshop.java
@@ -4,22 +4,29 @@ import java.util.Vector;
 import java.util.Scanner;
 
 public class App {
+    //this method is sort the array and show the array after sort
     private static void sort(String[] word){
+        //this algorithm is selection sort 
         for(int i = 0;i<word.length;i++){
+            // choose minimum value and position of minimumvalue first 
             String min = word[i];
             int p = i;
             for(int j=i;j<word.length;j++){
+                //then check if it really minmum value if it true remember the position
                 if(min.compareTo(word[j]) < 0){
                     min = word[j];
                     p = j;
                 }
             }
+            //last check if the first index is really minimum
             if(min.compareTo(word[i]) > 0){
                 String temp = word[i];
                 word[i] = word[p];
                 word[p] = temp;
             }
         }
+        
+        //output it 
         for(String i : word){
             System.out.println(i);
         }
@@ -33,7 +40,8 @@ public class App {
             input = in.nextLine();
             word.add(input);
         }while(!input.equals("quit"));
-
+           
+        //this line for remove "quit" out off the vector
         word.remove(word.size()-1);
 
         String[] arr = new String[word.size()];
