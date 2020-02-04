9c5936f8aa6dc6ace8d89061e5932d0d866bc467
Oralita <54722120+oat431@users.noreply.github.com>
2020-02-04 22:41:08 +0700

    check parentesis

--- Main.java
+++ Main.java
@@ -1,11 +1,60 @@
 package com.company;
-
+import java.util.Stack;
 public class Main {
-
+    private static boolean check(String word){
+        Stack<Character> chk = new Stack<>();
+        switch (word.charAt(0)){
+            case '{':
+            case '[':
+            case '(':
+                chk.push(word.charAt(0));
+                break;
+            default:
+                return false;
+        }
+        for(int i=1;i<word.length();i++){
+            for(char j : chk){
+                System.out.print(j);
+            }
+            System.out.println();
+            switch (word.charAt(i)){
+                case '{':
+                case '[':
+                case '(':
+                    chk.push(word.charAt(i));
+                    break;
+            }
+            switch (word.charAt(i)){
+                case '}':
+                    if(chk.peek() == '[' || chk.peek() == '('){
+                        return false;
+                    }
+                    chk.pop();
+                    break;
+                case ']':
+                    if(chk.peek() == '{' || chk.peek() == '('){
+                        return false;
+                    }
+                    chk.pop();
+                    break;
+                case ')':
+                    if(chk.peek() == '{' || chk.peek() == '['){
+                        return false;
+                    }
+                    chk.pop();
+                    break;
+            }
+        }
+        if(chk.empty()){
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        Basket oat = new Basket();
-        System.out.println(oat.getSize());
-        System.out.println(String.format("%.2f",oat.getCost()));
-        oat.getType();
+        String data = "{[(())]}";
+
+        System.out.println(check(data));
+
+
     }
 }
