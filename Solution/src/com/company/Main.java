f94cbe0c70b3406a466a820f21cc848e65ffb5d8
Oralita <54722120+oat431@users.noreply.github.com>
2020-02-11 21:56:35 +0700

    Add files via upload

---
+++ Solution/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+public class Main{
+
+    public static void main(String[] args){
+        Forest cmu = new Forest(100,15);
+        cmu.showTreeInTheForest();
+        cmu.ForestInformation();
+    }
+}
