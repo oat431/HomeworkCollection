5d4f9414b3ab414c1a071b360cef42dbeb7c6886
Oralita <54722120+oat431@users.noreply.github.com>
2020-09-15 17:20:22 +0700

    update Inteactive website

---
+++ 953261/personalInfo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my Info</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>hi my name is Sahachan Tippimwong</h1>";
+        echo "<h3>I born in Kamphanephet but now I live in chiang mai</h3>";
+        echo "<h5>I write this infomation in 15/9/2020</h5>"
+        //sahahchan Tippimwong 15/09/2020
+    ?> 
+</body>
+</html>
