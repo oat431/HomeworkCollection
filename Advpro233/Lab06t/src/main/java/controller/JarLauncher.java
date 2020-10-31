59e02552dee996749b6d7d8980cb3bd91fdac896
Oralita <54722120+oat431@users.noreply.github.com>
2020-11-01 00:40:04 +0700

    Project object model

---
+++ Advpro233/Lab06t/src/main/java/controller/JarLauncher.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class JarLauncher {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
---
+++ Advpro233/Lab06t/src/main/java/controller/Launcher.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.Food;
+import model.Snake;
+import view.Platform;
+public class Launcher extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Platform platform = new Platform();
+        Snake snake = new Snake(new Point2D(Platform.WIDTH/2,Platform.HEIGHT/2));
+        Food food = new Food();
+        GameLoop gameLoop = new GameLoop(platform,snake,food);
+        Scene scene = new Scene(platform,Platform.WIDTH * Platform.TILE_SIZE,Platform.HEIGHT * Platform.TILE_SIZE);
+        scene.setOnKeyPressed(e -> platform.setKey(e.getCode()));
+        scene.setOnKeyReleased(e -> platform.setKey(null));
+        primaryStage.setTitle("Snake");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        (new Thread(gameLoop)).start();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
