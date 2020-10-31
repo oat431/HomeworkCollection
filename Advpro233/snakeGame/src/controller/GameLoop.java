a027813ba47633e187c93af94bf37ad048c8426d
Oralita <54722120+oat431@users.noreply.github.com>
2020-10-31 18:40:48 +0700

    SnakeGame

---
+++ Advpro233/snakeGame/src/controller/GameLoop.java
@@ -0,0 +1,73 @@
+package controller;
+
+import javafx.scene.input.KeyCode;
+import model.Direction;
+import model.Food;
+import model.Snake;
+import view.Platform;
+
+public class GameLoop implements Runnable {
+    private Platform platform;
+    private Snake snake;
+    private Food food;
+    private float interval = 1000.0f / 10;
+    private boolean running;
+
+    public GameLoop(Platform platform, Snake snake, Food food){
+        this.snake = snake;
+        this.food = food;
+        this.platform = platform;
+        running = true;
+    }
+
+    public void update() {
+        KeyCode cur_key = platform.getKey();
+        Direction cur_direction = snake.getCurrentDirection();
+        if(cur_key == KeyCode.UP && cur_direction != Direction.DOWN){
+            snake.setCurrentDirection(Direction.UP);
+        } else if(cur_key == KeyCode.DOWN && cur_direction != Direction.UP){
+            snake.setCurrentDirection(Direction.DOWN);
+        } else if(cur_key == KeyCode.LEFT && cur_direction != Direction.RIGHT){
+            snake.setCurrentDirection(Direction.LEFT);
+        } else if(cur_key == KeyCode.RIGHT && cur_direction != Direction.LEFT){
+            snake.setCurrentDirection(Direction.RIGHT);
+        }
+        snake.update();
+    }
+
+    public void checkCollision() {
+        if(snake.isCollidingWith(food)){
+            snake.grow();
+            food.respawn();
+        }
+        if(snake.isDead()){
+            running = false;
+        }
+    }
+
+    private void redraw() {
+        platform.render(snake,food);
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public Platform getPlatform() {
+        return platform;
+    }
+
+    @Override
+    public void run() {
+        while(running) {
+            update();
+            checkCollision();
+            redraw();
+            try{
+                Thread.sleep((long)interval);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
