8edc8d40aa6f30a4148bacf040d70449ecb819d5
Oralita <54722120+oat431@users.noreply.github.com>
2021-02-19 15:18:01 +0700

    Update Node.java

--- ADT102/LinkedList/Node.java
+++ ADT102/LinkedList/Node.java
@@ -1,11 +1,10 @@
 public class Node{
     public Node next;
-    private Node prev;
     public Student data;
 
     public Node(Student data){
         this.data = data;   
-        next=prev=null;
+        next=null;
     }
 
 }
